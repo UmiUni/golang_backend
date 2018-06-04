2eb3ebc53174534210ffca3fc464bc2c9e3f7275
Mengxiong <mliu60@illinois.edu>
2018-06-03 17:12:45 -0700

    server

--- handler/handler.go
+++ handler/handler.go
@@ -1,12 +1,29 @@
-// This package is based on this great article about error handling with golang headers
-//   https://elithrar.github.io/article/http-handler-error-handling-revisited/
-
 package handler
 
 import (
-	"log"
 	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+	"log"
+	"errors"
+	"strconv"
+	"math/rand"
+	"code.jogchat.internal/jogchat_golang_backend/postgres"
 )
+
+// Creds holds the credentials we send back
+type Creds struct {
+	Status      string
+	APIKey      string
+	AccountType string
+	Email       string
+	AuthToken   string
+	IsLoggedIn  bool
+}
+
 
 // Env holds application-wide configuration.
 type Env struct {
@@ -67,4 +84,128 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				http.StatusInternalServerError)
 		}
 	}
+}
+
+
+func myLookupKey(key string) []byte {
+	return []byte(key)
+}
+
+
+
+func hasValidToken(jwtToken, key string) bool {
+	ret := false
+	token, err := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return myLookupKey(key), nil
+	})
+
+	if err == nil && token.Valid {
+		ret = true
+	}
+	return ret
+}
+
+
+// FakeData provides some fake data for testing...
+func FakeData(env *Env, w http.ResponseWriter, r *http.Request) error {
+
+	// If this is an OPTION method, then we don't do anything since it's just
+	// validating the preflight info
+	if r.Method == "OPTIONS" {
+		return nil
+	}
+
+	// Validate the API call
+	secret := r.Header.Get("x-authentication")
+	isValid := hasValidToken(secret, env.Secret)
+	if !isValid {
+		return StatusError{401, errors.New("Invalid authorization token")}
+	}
+
+	// If we get here, then we've got a valid call.  So, go ahead and
+	// process the request.  In this instance, all we want to do
+	// is pass back a list of integers that is N items long
+	N, err := strconv.Atoi(r.FormValue("N"))
+	if err != nil {
+		N = 10
+	}
+
+	// Create N random integers
+	rand.Seed(time.Now().UTC().UnixNano())
+	var data []int
+	for i := 0; i < N; i++ {
+		data = append(data, rand.Intn(100))
+	}
+
+	time.Sleep(1 * time.Second) // just for fun, pause a bit
+
+	// return the results
+	out, _ := json.MarshalIndent(&data, "", "  ")
+	fmt.Fprintf(w, string(out))
+	return nil
+}
+
+
+// GetCredentials determines if the username and password is valid
+// This is where logic would go to validate and return account info
+func GetCredentials(env *Env, username, password string) Creds {
+	credentials := Creds{
+		Status:      "UNAUTHORIZED",
+		APIKey:      "",
+		AccountType: "",
+		Email:       "",
+		AuthToken:   "",
+		IsLoggedIn:  false,
+	}
+	if (username == "admin") && (password == "admin") {
+		credentials.Status = "OK"
+		credentials.APIKey = "12345"
+		credentials.AccountType = "admin"
+		credentials.Email = "admin@example.com"
+		credentials.IsLoggedIn = true
+		// Now create a JWT for user
+		// Create the token
+		token := jwt.New(jwt.SigningMethodHS256)
+		// Set some claims
+		claims := token.Claims.(jwt.MapClaims)
+		claims["sub"] = username
+		claims["iss"] = "example.com"
+		claims["exp"] = time.Now().Add(time.Hour *72).Unix()
+		var err error
+		credentials.AuthToken, err = token.SignedString([]byte(env.Secret))
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	return credentials
+}
+
+
+// Login captures the data posted to the /login route
+func Login(env *Env, w http.ResponseWriter, r *http.Request) error {
+	data, _ := ioutil.ReadAll(r.Body) // Read the body of the POST request
+	// Unmarshall this into a map
+	var params map[string]string
+	json.Unmarshal(data, &params)
+
+	postgres.LoginDB(w, params)
+	credentials := GetCredentials(env, params["username"], params["password"])
+
+	out, _ := json.MarshalIndent(&credentials, "", "  ")
+	fmt.Fprintf(w, string(out))
+
+	return nil
+}
+
+func Signup(env *Env, w http.ResponseWriter, r *http.Request) error {
+	data, _ := ioutil.ReadAll(r.Body)
+	var params map[string]string
+	json.Unmarshal(data, &params)
+
+	postgres.SignupDB(w, params)
+	return nil
 }
